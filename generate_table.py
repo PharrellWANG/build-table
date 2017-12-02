c09661d8700561eca8aa6b2fa7e0f72079d762d0
Pharrell.zx <15113029g@connect.polyu.hk>
2017-12-02 16:16:50 +0800

    new txt files added for generating tables for step3 in pre-processing process.

--- generate_table.py
+++ generate_table.py
@@ -1,14 +1,14 @@
 from extract_text import ExtractText
 
 list_file_names = [
-    'txt/size_04.txt',
-    'txt/size_08.txt',
-    'txt/size_16.txt',
-    'txt/size_32.txt'
+    'txt/sm_04.txt',
+    'txt/sm_08.txt',
+    'txt/sm_16.txt',
+    'txt/sm_32.txt'
 ]
 
 # extraction1 = ExtractText(is_ordered=False)
-# extraction1.read_lines_into_list(file_name='txt/size_04.txt')
+# extraction1.read_lines_into_list(file_name='txt/sm_04.txt')
 # extraction1.print_lists()
 
 extraction2 = ExtractText(is_ordered=False)
@@ -16,7 +16,7 @@ extraction2.read_lines_into_multiple_lists(list_of_file_names=list_file_names)
 extraction2.print_full_lists()
 
 # extraction3 = ExtractText(is_ordered=True)
-# extraction3.read_lines_into_list(file_name='txt/size_04.txt')
+# extraction3.read_lines_into_list(file_name='txt/sm_04.txt')
 # extraction3.print_lists()
 
 extraction4 = ExtractText(is_ordered=True)
